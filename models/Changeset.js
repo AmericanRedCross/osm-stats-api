584c1d79b4b126ae0e7a116d740a55130980fae5
Marc Farra <marcfarra@gmail.com>
2016-01-14 23:25:58 -0500

    Add models

---
+++ models/Changeset.js
@@ -0,0 +1,20 @@
+var bookshelf = require('../db/bookshelf_init');
+require('./User');
+require('./Hashtag');
+require('./Country');
+
+// Returns Changeset model
+var Changeset = bookshelf.Model.extend({
+  tableName: 'changesets',
+  users: function () {
+    return this.belongsTo('User');
+  },
+  hashtags: function () {
+    return this.belongsToMany('Hashtag');
+  },
+  countries: function () {
+    return this.belongsToMany('Country');
+  }
+});
+
+module.exports = bookshelf.model('Changeset', Changeset);
