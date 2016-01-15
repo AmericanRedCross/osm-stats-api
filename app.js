580126ffeb12cbfdc7365adbb1469a6edce9ed7f
Marc Farra <marcfarra@gmail.com>
2016-01-15 00:08:57 -0500

    Add initial user route

--- app.js
+++
@@ -1,8 +0,0 @@
-var Hapi = require('hapi');
-
-var server = new Hapi.Server();
-server.connection({port: 3000});
-
-server.start(() => {
-  console.log('Server running at:', server.info.uri);
-});
---
+++ routes/user.js
@@ -0,0 +1,39 @@
+var Boom = require('boom');
+var User = require('../models/User');
+var bookshelf = require('../db/bookshelf_init')
+var Promise = require('bluebird');
+var R = require('ramda');
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/users/{id}',
+    handler: function (req, res) {
+      if (!req.params.id) {
+        return res(Boom.badRequest('Valid user id required'));
+      }
+      Promise.all([
+        User.where({id: req.params.id}).fetch({withRelated: 'badges'}),
+        bookshelf.knex('changesets')
+        .select('*')
+        .orderBy('created_at', 'desc')
+        .where('user_id', req.params.id)
+        .limit(1),
+        bookshelf.knex('changesets')
+        .select('created_at')
+        .where('user_id', req.params.id)
+      ])
+      .then(function (results) {
+        var user = results[0];
+        var latest = results[1];
+        var edit_times = R.map(R.prop('created_at'), results[2]);
+
+        var serialized = user.toJSON();
+        serialized.latest = latest;
+        serialized.edit_times = edit_times;
+        return serialized;
+      })
+      .then(res);
+    }
+  }
+]
