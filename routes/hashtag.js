f1c624fdf265aa6237bc2a046f3b7c0905be8fd1
Matthew Hanson <matt.a.hanson@gmail.com>
2017-03-17 13:28:47 -0400

    fix syntax err

--- routes/hashtag.js
+++ routes/hashtag.js
@@ -82,7 +82,7 @@ function allHashtagData (req, res) {
     })
     .then(res);
 }
-module.exports = [
+module.exports = [{
   // This method is taking a lot of memory and crashing
   // We disable it for now
   // {
