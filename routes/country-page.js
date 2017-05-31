62f3f4625cd5a492d43d7fb6be0fd408c78954b6
maxgrossman <m.grossman1813@gmail.com>
2017-05-31 18:22:35 -0400

    #20 add country hashtag route
    stepped on more thenable, now it returns the list we want...but still need to add the actual hashtag

--- routes/country-page.js
+++ routes/country-page.js
@@ -20,33 +20,34 @@ module.exports = [
         var country_id = results.rows[0].id
         return country_id
       }).then(function (country_id) {
-        var hashtag_ids = knex.raw("SELECT distinct hashtag_id FROM changesets_hashtags WHERE changeset_id IN \
-          (SELECT changeset_id FROM changesets_countries WHERE country_id = " + country_id + ");")
+        // join changesets with changesets_hashtags
+        // do that only where changesets_hashtags.id are those in a given country
+        // select all the changesets_ids,hashtag_ids for country
+        // next I want to join all in the
+        var hashtag_ids = knex.raw(
+          "SELECT \
+            SUM(road_count_add) AS road_count_add, \
+            SUM(road_count_mod) AS road_count_mod, \
+            SUM(building_count_add) AS building_count_add, \
+            SUM(building_count_mod) AS building_count_mod, \
+            SUM(waterway_count_add) AS waterway_count_add, \
+            SUM(poi_count_add) AS poi_count_add, \
+            SUM(road_km_add) AS road_km_add, \
+            SUM(road_km_mod) AS road_km_mod, \
+            SUM(waterway_km_add) AS waterway_km_add, \
+            filtered.hashtag_id\
+          FROM changesets JOIN\
+            (SELECT hashtag_id, changeset_id FROM changesets_hashtags \
+              WHERE changeset_id IN \
+              (SELECT changeset_id FROM changesets_countries WHERE country_id = " + country_id + ")) AS filtered \
+            ON changesets.id=filtered.changeset_id\
+            GROUP by filtered.hashtag_id;"
+          )
+//         var hashtag_ids = knex.raw("select distinct hashtag_id from changesets_hashtags where changeset_id \
+// in (select changeset_id from changesets_countries where country_id=171);")
         return hashtag_ids
-      }).then(
-        function (country_results) {
-          const country_hashtag_ids = country_results.rows.map(function(d) {return d.hashtag_id})
-          return country_hashtag_ids.map(function(d) {
-            return knex.raw("SELECT * from hashtags where id in (" + parseInt(d) + ");")
-            // knex.raw(
-
-            // "\
-            // SELECT \
-            //   SUM(road_count_add) AS road_count_add \
-            //   SUM(building_count_add) AS building_count_add \
-            //   SUM(building_count_mod) AS building_count_mod \
-            //   SUM(waterway_count_add) AS waterway_count_add \
-            //   SUM(poi_count_add) AS poi_count_add \
-            //   SUM(road_km_add) AS road_km_add \
-            //   SUM(road_km_mod) AS road_km_mod\
-            //   SUM(waterway_km_add) AS waterway_km_add \
-            // FROM changesets WHERE id IN \
-            //   (SELECT changeset_id FROM changesets_hashtags WHERE changeset_id IN \
-            //     (SELECT changeset_id FROM changeset_countries WHERE country_id=" + parseInt(171) + ")\
-            //     AND hashtag_id=" + parseInt(d) + ")"
-            // )
-          })
       })
+      .then(function(hashtag_id_results) {return hashtag_id_results.rows})
       .then(res)
     }
   }
