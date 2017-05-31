3191767f8505b5e88d192c781f5c12dcef4cffc2
maxgrossman <m.grossman1813@gmail.com>
2017-05-31 18:11:05 -0400

    #20 add country hashtag route
    returns stats for each hashtag within a given country. need to include actual hashtags in that given object

--- routes/country-page.js
+++ routes/country-page.js
@@ -22,27 +22,30 @@ module.exports = [
       }).then(function (country_id) {
         var hashtag_ids = knex.raw("SELECT distinct hashtag_id FROM changesets_hashtags WHERE changeset_id IN \
           (SELECT changeset_id FROM changesets_countries WHERE country_id = " + country_id + ");")
-        return [hashtag_ids,country_id]
+        return hashtag_ids
       }).then(
         function (country_results) {
-          var hashtag_ids_here = country_results[0]
-          // rows.map(function(d) {return d.hashtag_id})
-          // return hashtag_ids.map((d) => {return knex.raw("\
-          //   SELECT \
-          //     SUM(road_count_add) AS road_count_add \
-          //     SUM(building_count_add) AS building_count_add \
-          //     SUM(building_count_mod) AS building_count_mod \
-          //     SUM(waterway_count_add) AS waterway_count_add \
-          //     SUM(poi_count_add) AS poi_count_add \
-          //     SUM(road_km_add) AS road_km_add \
-          //     SUM(road_km_mod) AS road_km_mod\
-          //     SUM(waterway_km_add) AS waterway_km_add \
-          //   FROM changesets WHERE id IN \
-          //     (SELECT changeset_id FROM changesets_hashtags WHERE changeset_id IN \
-          //       (SELECT changeset_id FROM changeset_countries WHERE country_id=" + country_id + ")\
-          //       AND hashtag_id=" + d + ")")
-          // })
-          return hashtag_ids_here
+          const country_hashtag_ids = country_results.rows.map(function(d) {return d.hashtag_id})
+          return country_hashtag_ids.map(function(d) {
+            return knex.raw("SELECT * from hashtags where id in (" + parseInt(d) + ");")
+            // knex.raw(
+
+            // "\
+            // SELECT \
+            //   SUM(road_count_add) AS road_count_add \
+            //   SUM(building_count_add) AS building_count_add \
+            //   SUM(building_count_mod) AS building_count_mod \
+            //   SUM(waterway_count_add) AS waterway_count_add \
+            //   SUM(poi_count_add) AS poi_count_add \
+            //   SUM(road_km_add) AS road_km_add \
+            //   SUM(road_km_mod) AS road_km_mod\
+            //   SUM(waterway_km_add) AS waterway_km_add \
+            // FROM changesets WHERE id IN \
+            //   (SELECT changeset_id FROM changesets_hashtags WHERE changeset_id IN \
+            //     (SELECT changeset_id FROM changeset_countries WHERE country_id=" + parseInt(171) + ")\
+            //     AND hashtag_id=" + parseInt(d) + ")"
+            // )
+          })
       })
       .then(res)
     }
