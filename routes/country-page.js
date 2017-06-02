febe1ef7dbf27f0deb16e3ee1f6689f15430bc3a
maxgrossman <m.grossman1813@gmail.com>
2017-06-02 16:32:10 -0400

    #20 add hashtag route
    include # of contributors to total country stats route
    also, fix bug for querying for counties with multi-word names

--- routes/country-page.js
+++ routes/country-page.js
@@ -13,7 +13,7 @@ module.exports = [
       const knex = bookshelf.knex;
       // force title case
       const country = req.params.country.split(' ').map(l =>
-        l[0].toUpperCase() + l.substr(1).toLowerCase())
+        l[0].toUpperCase() + l.substr(1).toLowerCase()).join(" ")
       return knex.raw("select * FROM countries WHERE name IN ('" + country +"')")
       .then(function (results) {
         var country_id = results.rows[0].id
@@ -53,13 +53,57 @@ module.exports = [
       .then(res)
     }
   },
+  // return stats for entire country
+  {
+    method:'GET',
+    path: '/{country}',
+    handler: function(req, res) {
+      const knex = bookshelf.knex;
+      // force title case
+      const country = req.params.country.split(' ').map(l =>
+        l[0].toUpperCase() + l.substr(1).toLowerCase()).join(" ")
+      return knex.raw("select * FROM countries WHERE name IN ('" + country +"')")
+      .then(function (results) {
+        var country_id = results.rows[0].id
+        return country_id
+      }).then(function (country_id) {
+        var country_stats = knex.raw(
+          "SELECT \
+            SUM(building_count_add + building_count_mod + \
+                road_count_add + road_count_mod + \
+                waterway_count_add + poi_count_add) AS all_edits, \
+             SUM(road_count_add) AS road_count_add, \
+             SUM(road_count_mod) AS road_count_mod, \
+             SUM(building_count_add) AS building_count_add,  \
+             SUM(building_count_mod) AS building_count_mod,  \
+             SUM(waterway_count_add) AS waterway_count_add, \
+             SUM(poi_count_add) AS poi_count_add,  \
+             SUM(road_km_add) AS road_km_add,  \
+             SUM(road_km_mod) AS road_km_mod, \
+             COUNT(DISTINCT filtered.name) AS contributors\
+          FROM changesets JOIN \
+            (SELECT name, id FROM \
+              (SELECT users.name, changesets.id FROM users \
+              JOIN changesets ON users.id = changesets.user_id) AS filtered \
+            WHERE id IN \
+              (SELECT changeset_id FROM changesets_countries WHERE country_id = " + country_id + ")) \
+            AS filtered \
+          ON changesets.id=filtered.id;"
+        )
+        return country_stats
+      }).then(function(country_stats_results) {
+        return country_stats_results.rows
+      })
+      .then(res)
+    }
+  },
   {
     method: 'GET',
     path: "/{country}/users",
     handler: function(req, res) {
       const knex = bookshelf.knex
       const country = req.params.country.split(' ').map(l =>
-        l[0].toUpperCase() + l.substr(1).toLowerCase())
+        l[0].toUpperCase() + l.substr(1).toLowerCase()).join(" ")
       return knex.raw("select * FROM countries WHERE name IN ('" + country + "')")
       .then(function (results) {
         var country_id = results.rows[0].id
