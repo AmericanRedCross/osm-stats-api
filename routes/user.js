b56e93bf1c2a50d909476dcf774cff61355bceb1
Seth Fitzsimmons <seth@mojodna.net>
2017-09-29 16:28:01 -0700

    async/await + number formatting

--- routes/user.js
+++ routes/user.js
@@ -15,7 +15,7 @@ module.exports = [
   {
     method: "GET",
     path: "/users/{id}",
-    handler: (req, res) => {
+    handler: async (req, res) => {
       if (!req.params.id) {
         return res(Boom.badRequest("Valid user id required"));
       }
@@ -28,8 +28,8 @@ module.exports = [
         where.name = req.params.id;
       }
 
-      return User.where(where)
-        .fetch({
+      try {
+        const userObj = await User.where(where).fetch({
           withRelated: [
             {
               badges: qb =>
@@ -43,70 +43,98 @@ module.exports = [
             }
           ],
           require: true
-        })
-        .then(user =>
-          Promise.all([
-            user.getHashtags(),
-            bookshelf
-              .knex("changesets")
-              .select("id")
-              .orderBy("created_at", "desc")
-              .where("user_id", user.id)
-              .limit(1),
-            bookshelf
-              .knex("changesets")
-              .select("created_at")
-              .where("user_id", user.id),
-            user.getCountries(),
-            bookshelf
-              .knex("changesets")
-              .count("user_id")
-              .where("user_id", user.id)
-          ]).then(results => {
-            const hashtags = results[0];
-            const latest = results[1];
-            const editTimes = R.map(R.prop("created_at"), results[2]);
-            const countryCount = results[3][0];
-            const countryList = R.countBy(R.prop("name"), results[3][1]);
-            const changesetCount = results[4][0].count;
-
-            const serialized = user.toJSON();
-            serialized.changeset_count = changesetCount;
-            serialized.latest = latest[0].id;
-            serialized.edit_times = editTimes;
-            serialized.country_count = countryCount;
-            serialized.country_list = countryList;
-            serialized.hashtags = hashtags;
-            return serialized;
-          })
-        )
-        .then(serialized =>
-          Changeset.where({ id: serialized.latest })
-            .fetch({ withRelated: ["hashtags", "countries"] })
-            .then(changeset => {
-              changeset = changeset.toJSON();
-              changeset.hashtags = R.map(
-                R.pick(["id", "hashtag", "created_at"]),
-                changeset.hashtags
-              );
-              changeset.countries = R.map(
-                R.pick(["id", "name", "code", "created_at"]),
-                changeset.countries
-              );
-              serialized.latest = changeset;
-              return serialized;
-            })
-        )
-        .then(res)
-        .catch(err => {
-          if (err.message && err.message === "EmptyResponse") {
-            return res(Boom.notFound("user not found"));
-          }
-
-          console.warn(err);
-
-          return res(Boom.badImplementation("error retrieving user"));
         });
+
+        const [
+          hashtags,
+          latest,
+          changesetDates,
+          countries,
+          changesets
+        ] = await Promise.all([
+          userObj.getHashtags(),
+          bookshelf
+            .knex("changesets")
+            .select("id")
+            .orderBy("created_at", "desc")
+            .where("user_id", userObj.id)
+            .limit(1),
+          bookshelf
+            .knex("changesets")
+            .select("created_at")
+            .where("user_id", userObj.id),
+          userObj.getCountries(),
+          bookshelf
+            .knex("changesets")
+            .count("user_id")
+            .where("user_id", userObj.id)
+        ]);
+
+        const editTimes = R.map(R.prop("created_at"), changesetDates);
+        const countryCount = countries[0];
+        const countryList = R.countBy(R.prop("name"), countries[1]);
+        const changesetCount = changesets[0].count;
+        const user = userObj.toJSON({
+          omitPivot: true
+        });
+
+        const serialized = {
+          ...user,
+          total_building_count_add: Number(user.total_building_count_add),
+          total_building_count_mod: Number(user.total_building_count_mod),
+          total_waterway_count_add: Number(user.total_waterway_count_add),
+          total_poi_count_add: Number(user.total_poi_count_add),
+          total_road_km_add: Number(user.total_road_km_add),
+          total_road_km_mod: Number(user.total_road_km_mod),
+          total_waterway_km_add: Number(user.total_waterway_km_add),
+          total_josm_edit_count: Number(user.total_josm_edit_count),
+          total_gps_trace_count_add: Number(user.total_gps_trace_count_add),
+          total_road_count_add: Number(user.total_road_count_add),
+          total_road_count_mod: Number(user.total_road_count_mod),
+          total_tm_done_count: Number(user.total_tm_done_count),
+          total_tm_val_count: Number(user.total_tm_val_count),
+          total_tm_inval_count: Number(user.total_tm_inval_count),
+          changeset_count: changesetCount,
+          latest: latest[0].id,
+          edit_times: editTimes,
+          country_count: countryCount,
+          country_list: countryList,
+          hashtags
+        };
+
+        const changesetObj = await Changeset.where({
+          id: serialized.latest
+        }).fetch({ withRelated: ["hashtags", "countries"] });
+
+        const changeset = changesetObj.toJSON({
+          omitPivot: true
+        });
+
+        serialized.latest = {
+          ...changeset,
+          road_count_add: Number(changeset.road_count_add),
+          road_count_mod: Number(changeset.road_count_mod),
+          building_count_add: Number(changeset.building_count_add),
+          building_count_mod: Number(changeset.building_count_mod),
+          waterway_count_add: Number(changeset.waterway_count_add),
+          poi_count_add: Number(changeset.poi_count_add),
+          gpstrace_count_add: Number(changeset.gpstrace_count_add),
+          road_km_add: Number(changeset.road_km_add),
+          road_km_mod: Number(changeset.road_km_mod),
+          waterway_km_add: Number(changeset.waterway_km_add),
+          gpstrace_km_add: Number(changeset.gpstrace_km_add)
+        };
+
+        return res(serialized);
+      } catch (err) {
+        if (err.message && err.message === "EmptyResponse") {
+          return res(Boom.notFound("user not found"));
+        }
+
+        console.warn(err);
+
+        return res(Boom.badImplementation("error retrieving user"));
+      }
     }
   }
 ];
