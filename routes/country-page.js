e442afc3aa1f634d5dedb167de0fc964100d2f81
Seth Fitzsimmons <seth@mojodna.net>
2017-09-29 18:13:13 -0700

    countries: async/await + date filtering

--- routes/country-page.js
+++ routes/country-page.js
@@ -7,115 +7,160 @@ module.exports = [
   {
     method: "GET",
     path: "/countries",
-    handler: (req, res) => {
+    handler: async (req, res) => {
       const { knex } = bookshelf;
 
-      return knex("countries")
-        .select("name", "code")
-        .where(knex.raw("code !~'USA-'"))
-        .then(res);
+      try {
+        return res(
+          await knex("countries")
+            .select("name", "code")
+            .where(knex.raw("code !~'USA-'"))
+        );
+      } catch (err) {
+        return res(err);
+      }
     }
   },
   // returns stats for all hashtags in a country
   {
     method: "GET",
     path: "/countries/{country}/hashtags",
-    handler: (req, res) => {
+    handler: async (req, res) => {
       const { knex } = bookshelf;
       // force title case
       const country = req.params.country.toUpperCase();
-      return knex("countries")
-        .where("code", country)
-        .then(results => {
-          if (results.length === 0) {
-            throw new RecordNotFound();
-          }
-          return results[0].id;
-        })
-        .then(countryId =>
-          // for given country_id
-          // returns stats for all hashtags and name of hashtag
-          knex
-            .select(
-              knex.raw(
-                "SUM(building_count_add + building_count_mod + road_count_add + road_count_mod + waterway_count_add + poi_count_add) AS all_edits"
+
+      let startDate = new Date(0);
+      let endDate = new Date();
+
+      if (req.query.startdate != null) {
+        startDate = new Date(req.query.startdate);
+      }
+
+      if (req.query.enddate != null) {
+        endDate = new Date(req.query.enddate);
+      }
+
+      try {
+        const results = await knex("countries").where("code", country);
+
+        if (results.length === 0) {
+          throw new RecordNotFound();
+        }
+
+        const countryId = results[0].id;
+
+        // for given country_id
+        // returns stats for all hashtags and name of hashtag
+        const rows = await knex
+          .select(
+            knex.raw(
+              "SUM(building_count_add + building_count_mod + road_count_add + road_count_mod + waterway_count_add + poi_count_add) AS all_edits"
+            )
+          )
+          .sum("road_count_add AS road_count_add")
+          .sum("road_count_mod AS road_count_mod")
+          .sum("building_count_add AS building_count_add")
+          .sum("building_count_mod AS building_count_mod")
+          .sum("waterway_count_add AS waterway_count_add")
+          .sum("poi_count_add AS poi_count_add")
+          .sum("road_km_add AS road_km_add")
+          .sum("road_km_mod AS road_km_mod")
+          .sum("waterway_km_add AS waterway_km_add")
+          .select("filtered.hashtag")
+          .from("changesets")
+          .innerJoin(
+            knex
+              .select("hashtag", "id", "changeset_id")
+              .from(
+                knex
+                  .select(
+                    "hashtags.id",
+                    "hashtags.hashtag",
+                    "changesets_hashtags.changeset_id"
+                  )
+                  .from("hashtags")
+                  .innerJoin(
+                    "changesets_hashtags",
+                    "hashtags.id",
+                    "changesets_hashtags.hashtag_id"
+                  )
+                  .as("hashtag_changesets_joined")
               )
-            )
-            .sum("road_count_add AS road_count_add")
-            .sum("road_count_mod AS road_count_mod")
-            .sum("building_count_add AS building_count_add")
-            .sum("building_count_mod AS building_count_mod")
-            .sum("waterway_count_add AS waterway_count_add")
-            .sum("poi_count_add AS poi_count_add")
-            .sum("road_km_add AS road_km_add")
-            .sum("road_km_mod AS road_km_mod")
-            .sum("waterway_km_add AS waterway_km_add")
-            .select("filtered.hashtag")
-            .from("changesets")
-            .innerJoin(
-              knex
-                .select("hashtag", "id", "changeset_id")
-                .from(
-                  knex
-                    .select(
-                      "hashtags.id",
-                      "hashtags.hashtag",
-                      "changesets_hashtags.changeset_id"
-                    )
-                    .from("hashtags")
-                    .innerJoin(
-                      "changesets_hashtags",
-                      "hashtags.id",
-                      "changesets_hashtags.hashtag_id"
-                    )
-                    .as("hashtag_changesets_joined")
-                )
-                .whereIn(
-                  "changeset_id",
-                  knex
-                    .select("changeset_id")
-                    .from("changesets_countries")
-                    .where({
-                      country_id: countryId
-                    })
-                )
-                .as("filtered"),
-              "changesets.id",
-              "filtered.changeset_id"
-            )
-            .groupBy("filtered.hashtag")
-        )
-        .then(res)
-        .catch(error => {
-          if (error instanceof RecordNotFound) {
-            return res(Boom.notFound("Could not find country with that id"));
-          }
+              .whereIn(
+                "changeset_id",
+                knex
+                  .select("changeset_id")
+                  .from("changesets_countries")
+                  .where({
+                    country_id: countryId
+                  })
+              )
+              .as("filtered"),
+            "changesets.id",
+            "filtered.changeset_id"
+          )
+          .whereBetween("created_at", [startDate, endDate])
+          .groupBy("filtered.hashtag");
 
-          console.warn(error);
+        return res(
+          rows.map(x => ({
+            ...x,
+            all_edits: Number(x.all_edits),
+            road_count_add: Number(x.road_count_add),
+            road_count_mod: Number(x.road_count_mod),
+            building_count_add: Number(x.building_count_add),
+            building_count_mod: Number(x.building_count_mod),
+            waterway_count_add: Number(x.waterway_count_add),
+            poi_count_add: Number(x.poi_count_add),
+            road_km_add: Number(x.road_km_add),
+            road_km_mod: Number(x.road_km_mod),
+            waterway_km_add: Number(x.waterway_km_add)
+          }))
+        );
+      } catch (err) {
+        if (err instanceof RecordNotFound) {
+          return res(Boom.notFound("Could not find country with that id"));
+        }
 
-          return res(Boom.badImplementation("An unexpected error occurred"));
-        });
+        console.warn(err);
+
+        return res(Boom.badImplementation("An unexpected error occurred"));
+      }
     }
   },
   // return stats for entire country
   {
     method: "GET",
     path: "/countries/{country}",
-    handler: (req, res) => {
+    handler: async (req, res) => {
       const { knex } = bookshelf;
+
+      let startDate = new Date(0);
+      let endDate = new Date();
+
+      if (req.query.startdate != null) {
+        startDate = new Date(req.query.startdate);
+      }
+
+      if (req.query.enddate != null) {
+        endDate = new Date(req.query.enddate);
+      }
+
       // force title case
       const country = req.params.country.toUpperCase();
-      return knex("countries")
-        .where("code", country)
-        .then(results => {
-          if (results.length === 0) {
-            throw new RecordNotFound();
-          }
-          return results[0].id;
-        })
-        .then(countryId =>
-          knex.raw(
-            `
+
+      try {
+        let results = await knex("countries").where("code", country);
+
+        if (results.length === 0) {
+          throw new RecordNotFound();
+        }
+
+        const countryId = results[0].id;
+
+        results = await knex.raw(
+          `
               SELECT
                 SUM(building_count_add + building_count_mod +
                     road_count_add + road_count_mod +
@@ -134,44 +179,70 @@ module.exports = [
                   (SELECT users.name, changesets.id FROM users
                   JOIN changesets ON users.id = changesets.user_id) AS filtered
                 WHERE id IN
-                  -- TODO SQL injection
                   (SELECT changeset_id FROM changesets_countries WHERE country_id = ?))
                 AS filtered
               ON changesets.id=filtered.id
+              WHERE changesets.created_AT BETWEEN ? AND ?
           `,
-            [countryId]
-          )
-        )
-        .then(countryStatsResults => countryStatsResults.rows[0])
-        .then(res)
-        .catch(error => {
-          if (error instanceof RecordNotFound) {
-            return res(Boom.notFound("Could not find country with that id"));
-          }
+          [countryId, startDate, endDate]
+        );
 
-          console.warn(error);
+        const stats = results.rows[0];
 
-          return res(Boom.badImplementation("An unexpected error occurred"));
+        return res({
+          ...stats,
+          all_edits: Number(stats.all_edits),
+          road_count_add: Number(stats.road_count_add),
+          road_count_mod: Number(stats.road_count_mod),
+          building_count_add: Number(stats.building_count_add),
+          building_count_mod: Number(stats.building_count_mod),
+          waterway_count_add: Number(stats.waterway_count_add),
+          poi_count_add: Number(stats.poi_count_add),
+          road_km_add: Number(stats.road_km_add),
+          road_km_mod: Number(stats.road_km_mod),
+          contributors: Number(stats.contributors)
         });
+      } catch (err) {
+        if (err instanceof RecordNotFound) {
+          return res(Boom.notFound("Could not find country with that id"));
+        }
+
+        console.warn(err);
+
+        return res(Boom.badImplementation("An unexpected error occurred"));
+      }
     }
   },
   {
     method: "GET",
     path: "/countries/{country}/users",
-    handler: (req, res) => {
+    handler: async (req, res) => {
       const { knex } = bookshelf;
+
+      let startDate = new Date(0);
+      let endDate = new Date();
+
+      if (req.query.startdate != null) {
+        startDate = new Date(req.query.startdate);
+      }
+
+      if (req.query.enddate != null) {
+        endDate = new Date(req.query.enddate);
+      }
+
       const country = req.params.country.toUpperCase();
-      return knex("countries")
-        .where("code", country)
-        .then(results => {
-          if (results.length === 0) {
-            throw new RecordNotFound();
-          }
-          return results[0].id;
-        })
-        .then(countryId =>
-          knex.raw(
-            `SELECT
+
+      try {
+        let results = await knex("countries").where("code", country);
+
+        if (results.length === 0) {
+          throw new RecordNotFound();
+        }
+
+        const countryId = results[0].id;
+
+        results = await knex.raw(
+          `SELECT
             SUM(building_count_add + building_count_mod +
                 road_count_add + road_count_mod +
                 waterway_count_add + poi_count_add) AS all_edits,
@@ -193,22 +264,35 @@ module.exports = [
               (SELECT changeset_id FROM changesets_countries WHERE country_id = ?))
             AS filtered
           ON changesets.id=filtered.id
+          WHERE changesets.created_at BETWEEN ? AND ?
           GROUP by filtered.name, filtered.user_id
         `,
-            [countryId]
-          )
-        )
-        .then(userIdsResults => userIdsResults.rows)
-        .then(res)
-        .catch(error => {
-          if (error instanceof RecordNotFound) {
-            return res(Boom.notFound("Could not find country with that id"));
-          }
+          [countryId, startDate, endDate]
+        );
 
-          console.warn(error);
+        return res(
+          results.rows.map(x => ({
+            ...x,
+            all_edits: Number(x.all_edits),
+            road_count_add: Number(x.road_count_add),
+            road_count_mod: Number(x.road_count_mod),
+            building_count_add: Number(x.building_count_add),
+            building_count_mod: Number(x.building_count_mod),
+            waterway_count_add: Number(x.waterway_count_add),
+            poi_count_add: Number(x.poi_count_add),
+            road_km_add: Number(x.road_km_add),
+            road_km_mod: Number(x.road_km_mod)
+          }))
+        );
+      } catch (err) {
+        if (err instanceof RecordNotFound) {
+          return res(Boom.notFound("Could not find country with that id"));
+        }
 
-          return res(Boom.badImplementation("An unexpected error occurred"));
-        });
+        console.warn(err);
+
+        return res(Boom.badImplementation("An unexpected error occurred"));
+      }
     }
   }
 ];
